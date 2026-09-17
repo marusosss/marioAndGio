2cd2d94e6ee9c3d582ed28656d8560b349d82a76
marusosss <caracciolo501@gmail.com>
2026-09-17 16:32:37 +0200

    Aggiunta del file java

---
+++ HelloController.java
@@ -0,0 +1,15 @@
+package com.example.demo_kubernates.controller;
+
+import org.springframework.http.MediaType;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
+    public String hello() {
+        return "<h1>Ciao Mario!</h1>" +
+                "<img src=\"/images/mario.gif\" alt=\"gif di Mario\" />";
+    }
+}
