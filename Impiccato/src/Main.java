fa56c910b7c3cf0727b21edca90d0c4af1f4fd34
Cynicalip <giovij50@gmail.com>
2026-09-17 17:16:05 +0200

    Inserito input da tastiera Devo trovare un metodo per ottenere la posizione nel foreach

--- Impiccato/src/Main.java
+++ Impiccato/src/Main.java
@@ -1,11 +1,18 @@
+import java.util.Scanner;
+
+
 public class Main {
     public static void main(String[] args) {
+
+        Scanner keyboard = new Scanner(System.in);
 
         String[] parole = {"rottonculo", "agrumi", "arancina", "pisnelo"};
 
 
         System.out.printf("Hello and welcome!");
 
+        System.out.println("Try a letter");
+        char cin = keyboard.nextLine().charAt(0);
 
         for (int i = 0; i<parole.length; i++){
             for (char c : parole[i].toCharArray()){
