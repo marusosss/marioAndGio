5507a48dd2a2ddd9ca0568feaa1e9aa5762467dd
Cynicalip <giovij50@gmail.com>
2026-09-17 16:37:53 +0200

    modifica Gio

--- HelloController.java
+++ HelloController.java
@@ -9,7 +9,7 @@ public class HelloController {
 
     @GetMapping(value = "/", produces = MediaType.TEXT_HTML_VALUE)
     public String hello() {
-        return "<h1>Ciao Mario!</h1>" +
-                "<img src=\"/images/mario.gif\" alt=\"gif di Mario\" />";
+        return "<h1>Ciao Giovanni!</h1>" +
+                "<img src=\"/images/mario.gif\" alt=\"gif di Mario Peto\" />";
     }
 }
